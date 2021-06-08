4415b869a0257e3e7eff35532125494e94b2f7d7
minhquoc5599 <minhquoc5599@gmail.com>
2021-06-08 14:51:41 +0700

    add: Header, MenuBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './parts/containers/Header/Header';
+import MenuBar from './parts/containers/MenuBar/MenuBar';
 
 const App = () => {
   return (
-    <div className="App">
-      Welcom to React
+    <div>
+      <MenuBar />
+      <Header />
+      
     </div>
+
   );
 }
 
---
+++ src/parts/containers/MenuBar/MenuBar.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const MenuBar = () => {
+  return (
+    <div className="left-sidebar-pro">
+      <nav id="sidebar" className>
+        <div className="sidebar-header">
+          <a href="index.html"><img className="main-logo" src="img/logo/logo.png" alt="a" /></a>
+          <strong><a href="index.html"><img src="img/logo/logosn.png" alt="a" /></a></strong>
+        </div>
+        <div className="left-custom-menu-adp-wrap comment-scrollbar">
+          <nav className="sidebar-nav left-sidebar-menu-pro">
+            <ul className="metismenu" id="menu1">
+              {/* admin */}
+              <li>
+                <a href="/">
+                  <span className="educate-icon educate-home icon-wrap" />
+                  <span className="mini-click-non">Trang chủ</span>
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <span className="educate-icon educate-professor icon-wrap" />
+                  <span className="mini-click-non">Giáo viên</span></a>
+              </li>
+              <li>
+                <a href="/">
+                  <span className="educate-icon educate-student icon-wrap" />
+                  <span className="mini-click-non">Học viên</span></a>
+              </li>
+              <li>
+                <a href="/">
+                  <span className="educate-icon educate-library icon-wrap" />
+                  <span className="mini-click-non">Lĩnh vực</span></a>
+              </li>
+              {/* teacher, admin */}
+              <li>
+                <a href="/">
+                  <span className="educate-icon educate-course icon-wrap"/> 
+                  <span className="mini-click-non">Khóa học</span></a>
+              </li>
+
+              {/* user */}
+              <li>
+                <a className="has-arrow" data-toggle="collapse" href="#collapseRoot" role="button" aria-expanded="false" aria-controls="collapseRoot">
+                  <span className="educate-icon educate-library icon-wrap"></span>
+                  <span className="mini-click-non">Lĩnh vực</span></a>
+                <ul className="submenu-angle" id="collapseRoot" className="collapse">
+                  <li><a className="has-arrow" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample1">
+                    <span className="mini-sub-pro">Lập trình</span></a>
+                    <ul className="submenu-angle" id="collapseExample1" className="collapse">
+                      <li>
+                        <li><a href="/"><span className="mini-sub-pro">Lập trình mobile</span></a></li>
+                        <li><a href="/"><span className="mini-sub-pro">Lập trình web</span></a></li>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="/"><span className="mini-sub-pro">Khoa học tự nhiên</span></a></li>
+                  <li><a href="/"><span className="mini-sub-pro">Khoa học xã hội</span></a></li>
+                </ul>
+              </li>
+              <li id="removable">
+                <a href="/logout"><span
+                  className="fa fa-sign-in icon-wrap" />
+                  <span className="mini-click-non">Đăng nhập</span></a>
+              </li>
+              <li id="removable">
+                <a href="/logout"><span
+                  className="fa fa-user-plus icon-wrap" />
+                  <span className="mini-click-non">Đăng ký</span></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default MenuBar
