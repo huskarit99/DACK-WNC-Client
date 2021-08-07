6c525155d6d4f6f5dac8e5451eaf8564ab4a9d3c
Thai Hoc Nguyen <hocnt@techvn.mti.co.jp>
2021-08-07 16:00:42 +0700

    MOD: no clearly

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -7,41 +7,42 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import MenuBar from "./parts/containers/MenuBar/MenuBar";
 import PrivateRoute from "./parts/components/PrivateRoute/PrivateRoute";
-import { appendScript } from './utils/appendScripts';
+import { appendScript } from "./utils/appendScripts";
 import CoursesListByCategory from "./pages/CoursesListByCategory/CoursesListByCategory";
 import CourseDetail from "./pages/CourseDetail/CourseDetail";
-import CoursesListBySearch from './pages/CoursesListBySearch/CoursesListBySearch';
+import CoursesListBySearch from "./pages/CoursesListBySearch/CoursesListBySearch";
+import PersonalInfo from "./pages/User/PersonalInfo/PersonalInfo";
 
 const App = () => {
   useEffect(() => {
-    appendScript('/js/jquery-price-slider.js');
-    appendScript('/js/counterup/jquery.counterup.min.js');
-    appendScript('/js/counterup/waypoints.min.js');
-    appendScript('/js/counterup/counterup-active.js');
-    appendScript('/js/metisMenu/metisMenu.min.js');
-    appendScript('/js/metisMenu/metisMenu-active.js');
-    appendScript('/js/morrisjs/raphael-min.js');
-    appendScript('/js/morrisjs/morris.js');
-    appendScript('/js/morrisjs/morris-active.js');
-    appendScript('/js/sparkline/jquery.sparkline.min.js');
-    appendScript('/js/sparkline/jquery.charts-sparkline.js');
-    appendScript('/js/sparkline/sparkline-active.js');
-    appendScript('/js/calendar/moment.min.js');
-    appendScript('/js/calendar/fullcalendar.min.js');
-    appendScript('/js/calendar/fullcalendar-active.js');
-    appendScript('/js/jquery.maskedinput.min.js');
-    appendScript('/js/masking-active.js');
-    appendScript('/js/datepicker/jquery-ui.min.js');
-    appendScript('/js/datepicker/datepicker-active.js');
-    appendScript('/js/form-validation/jquery.form.min.js');
-    appendScript('/js/form-validation/jquery.validate.min.js');
-    appendScript('/js/form-validation/form-active.js');
-    appendScript('/js/dropzone/dropzone.js');
-    appendScript('/js/summernote/summernote.min.js');
-    appendScript('/js/summernote/summernote-active.js');
-    appendScript('/js/icheck/icheck.min.js');
-    appendScript('/js/icheck/icheck-active.js');
-    appendScript('/js/main.js');
+    appendScript("/js/jquery-price-slider.js");
+    appendScript("/js/counterup/jquery.counterup.min.js");
+    appendScript("/js/counterup/waypoints.min.js");
+    appendScript("/js/counterup/counterup-active.js");
+    appendScript("/js/metisMenu/metisMenu.min.js");
+    appendScript("/js/metisMenu/metisMenu-active.js");
+    appendScript("/js/morrisjs/raphael-min.js");
+    appendScript("/js/morrisjs/morris.js");
+    appendScript("/js/morrisjs/morris-active.js");
+    appendScript("/js/sparkline/jquery.sparkline.min.js");
+    appendScript("/js/sparkline/jquery.charts-sparkline.js");
+    appendScript("/js/sparkline/sparkline-active.js");
+    appendScript("/js/calendar/moment.min.js");
+    appendScript("/js/calendar/fullcalendar.min.js");
+    appendScript("/js/calendar/fullcalendar-active.js");
+    appendScript("/js/jquery.maskedinput.min.js");
+    appendScript("/js/masking-active.js");
+    appendScript("/js/datepicker/jquery-ui.min.js");
+    appendScript("/js/datepicker/datepicker-active.js");
+    appendScript("/js/form-validation/jquery.form.min.js");
+    appendScript("/js/form-validation/jquery.validate.min.js");
+    appendScript("/js/form-validation/form-active.js");
+    appendScript("/js/dropzone/dropzone.js");
+    appendScript("/js/summernote/summernote.min.js");
+    appendScript("/js/summernote/summernote-active.js");
+    appendScript("/js/icheck/icheck.min.js");
+    appendScript("/js/icheck/icheck-active.js");
+    appendScript("/js/main.js");
   }, []);
   return (
     <BrowserRouter>
@@ -53,6 +54,7 @@ const App = () => {
           <PrivateRoute exact path="/profile" component={Profile} /> */}
           <PrivateRoute exact path="/login" component={Login} />
           <PrivateRoute exact path="/register" component={Register} />
+          <PrivateRoute exact path="/profile" component={PersonalInfo} />
           <Route exact path="/courses" component={CoursesListByCategory} />
           <Route exact path="/course/:id" component={CourseDetail} />
           <Route exact path="/courses/search" component={CoursesListBySearch} />
--- src/pages/CoursesListByCategory/CoursesListByCategory.jsx
+++ src/pages/CoursesListByCategory/CoursesListByCategory.jsx
@@ -1,23 +1,22 @@
-import { useRecoilValue } from 'recoil';
-import { useLocation } from 'react-router';
-import React, { useEffect, useState, Fragment } from 'react';
+import { useLocation } from "react-router";
+import React, { useEffect, useState } from "react";
 
-import { getAllByCategoryId } from '../../services/api/courseApi';
-import CourseItem from '../../parts/components/Courses/CourseItem';
-import Pagination from './containers/Pagination/Pagination';
+import { getAllByCategoryId } from "../../services/api/courseApi";
+import CourseItem from "../../parts/components/Courses/CourseItem";
+import Pagination from "./containers/Pagination/Pagination";
 
 const CoursesListByCategory = () => {
   let location = useLocation();
   let params = new URLSearchParams(location.search);
   const categoryid = params.get("categoryid");
-  const page = Number(params.get("page")) || 1 ;
+  const page = Number(params.get("page")) || 1;
   const [courses, setCourses] = useState(null);
 
   useEffect(() => {
     if (categoryid) {
-      getAllByCategoryId({categoryid, page}).then(result => {
+      getAllByCategoryId({ categoryid, page }).then((result) => {
         setCourses(result);
-      })
+      });
     }
   }, []);
 
@@ -25,20 +24,22 @@ const CoursesListByCategory = () => {
     <div className="courses-area">
       <div className="container-fluid">
         <div className="row">
-          {courses && courses.courses && courses.courses.map((course, index) => {
-            return <CourseItem course={course} key={index} />
-          })}
+          {courses &&
+            courses.courses &&
+            courses.courses.map((course, index) => {
+              return <CourseItem course={course} key={index} />;
+            })}
         </div>
         <div className="mg-b-30">
           <Pagination
             page={page}
             categoryid={categoryid}
-            page_number={courses && courses.page_number} />
+            page_number={courses && courses.page_number}
+          />
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default CoursesListByCategory
+export default CoursesListByCategory;
