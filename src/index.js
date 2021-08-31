3deb345b1d4753e73036cb82e4e42165459173ec
Thai Hoc Nguyen <hocnt@techvn.mti.co.jp>
2021-08-31 08:46:19 +0700

    MOD: fix deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import './index.css';
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
